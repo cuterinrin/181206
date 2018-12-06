539c2fc03851fc8a8ec780518402490491d2a71d
cuterinrin <yro7045@naver.com>
2018-12-06 15:41:58 +0900

    3

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 	typedef	struct link{ //typedef사용해서 이 것들을 한 단어로 대체 가능 
 		
 		int data;
@@ -11,5 +14,22 @@
 	static 전역변수 선언이지만 내 파일 내에서만 사용할 수 있다. 
 	extern 전역변수 선언, 다른 파일에서 전역변수 가져오기 가능*/
 	
+	link_ty* create_node(int value)//노드 생성 함수, 포인터 선언 
+	{
+	link_ty* ndptr;
+	//동적 메모리 할당 - 함수 맬록(구조체 크기)) 
+	ndptr = (link_ty*)malloc( sizeof(link_ty) );//명시적 형변환, sizeof함수 = link_ty의 크기
+	//오류 핸들링, 런타임 에러 방지 
+	if (ndptr == NULL)
+	{
+		printf("error!\n");
+		return NULL;
+	 } 
+	//정수값 저장 
+	ndptr->data = value;
+	ndptr->next = NULL; //중요 
 	
+	return ndptr;
+	
+	}
 	
